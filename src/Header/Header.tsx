caf9edaf7f6ed7dcdeb06a288feba509f0ea39af
Dmitry <Dmitry.Gorbachev@t-systems.com>
2022-05-07 21:27:16 +0300

    Create Header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+import {ReactComponent as TomatoIcon} from '../icons/tomato-icon.svg';
+import {ReactComponent as StatisticsIcon} from '../icons/statistics.svg';
+export function Header() {
+  return (
+    <header className={'header'}>
+      <div className={'container'}>
+        <TomatoIcon/>
+        <span className={'title'}>Pomodoro-GO</span>
+      </div>
+      <div className={'container'}>
+        <StatisticsIcon/>
+        <span className={'statistics'}>Статистика</span>
+      </div>
+    </header>
+  );
+}
